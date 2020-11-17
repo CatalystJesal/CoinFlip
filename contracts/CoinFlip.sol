ed22b349bc736eca3fe29ebd971023b7d9678342
CatalystJesal <shadowii.jp@gmail.com>
2020-11-17 20:15:40 +0000

    initial commit

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -1,66 +1,185 @@
 import "./Ownable.sol";
+import "./provableAPI.sol";
 import "./Destroyable.sol";
 import "./SafeMath.sol";
 pragma solidity 0.5.12;
 
 
-contract CoinFlip is Ownable, Destroyable {
+contract CoinFlip is Ownable, Destroyable, usingProvable {
 
 
 using SafeMath for uint256;
 
-uint private spend = 1 ether;
+struct Bet {
+address player;
+uint value;
+}
 
-uint public outcome = 0;
+struct Player {
+uint winnings;
+uint wins;
+uint loss;
+uint latestGuess;
+uint outcome;
+}
+
+uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
+
+uint private spend = 0.1 ether;
 
 uint public balance;
 
-mapping (address => uint) private stake;
+string public outcomeMessage;
 
-event stakeOutcome(uint value, string outcome);
+mapping (address => Player) private players;
+mapping (bytes32 => Bet) private waiting;
+
+
+event playersOutcome(uint value, string outcome);
+event LogNewProvableQuery(string description);
+event generatedRandomNumber(uint256 randomNumber);
+event playerStats(uint winnings, uint wins, uint loss);
 
  modifier minimumCost(uint cost) {
      require(msg.value >= spend, "Must send at least 1 ether to the contract");
      _;
 }
 
- constructor() public{
+ constructor() public {
      balance = 0;
+     provable_setProof(proofType_Ledger);
  }
 
 
- function random() private returns(uint){
-     return outcome = now % 2;
- }
+function getPlayer() public view returns(uint, uint, uint, uint, uint){
+    return(players[msg.sender].winnings, players[msg.sender].wins, players[msg.sender].loss, players[msg.sender].latestGuess, players[msg.sender].outcome);
+
+}
 
 
-function guess(uint number) public payable minimumCost(spend) {
-    require(number == 0 || number == 1, "please pick a a whole number between 0 and 1");
-    require(balance >= msg.value, "contract has insufficient funds for a payout in the event of a win");
+ function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+  require(msg.sender == provable_cbAddress());
 
-    stake[msg.sender] = msg.value;
+   if (provable_randomDS_proofVerify__returnCode(_queryId, _result, _proof) != 0) {
+            /**
+             * @notice  The proof verification has failed! Handle this case
+             *          however you see fit.
+             */
+        } else {
 
-    balance = balance.add(stake[msg.sender]);
+            players[waiting[_queryId].player].outcome = uint256(keccak256(abi.encodePacked(_result))) % 2;
 
-    if(number == random()){
 
-        uint winnings = stake[msg.sender].mul(2);
+              //FOR TESTING ONLY
+            /* emit generatedRandomNumber(players[waiting[_queryId].player].outcome); */
 
-         msg.sender.transfer(winnings);
+            delete waiting[_queryId];
 
-         balance -= winnings;
+      }
+}
 
-         assert(number == random());
-         emit stakeOutcome(winnings, "Congratulations! You have doubled your stake!");
+function update(uint guess) payable public
+{
+  uint256 QUERY_EXECUTION_DELAY = 0; // NOTE: The datasource currently does not support delays > 0!
+  uint256 GAS_FOR_CALLBACK = 200000;
 
-    } else {
-      assert(number != random());
-      emit stakeOutcome(stake[msg.sender], "You have lost your stake!");
-    }
+  //FOR PRODUCTION
+  bytes32 queryId = provable_newRandomDSQuery(
+      QUERY_EXECUTION_DELAY,
+      NUM_RANDOM_BYTES_REQUESTED,
+      GAS_FOR_CALLBACK
+  );
 
-    assert(outcome == 0 || outcome == 1);
-    assert(balance >= msg.value);
 
+  Bet memory b;
+
+  b.player = msg.sender;
+  b.value = msg.value;
+  waiting[queryId] = b;
+
+  players[msg.sender].latestGuess = guess;
+
+
+  assert(
+      keccak256(
+          abi.encodePacked(b.player, b.value))
+          ==
+          keccak256(
+              abi.encodePacked(waiting[queryId].player, waiting[queryId].value))
+        );
+
+  //FOR TESTING ONLY
+//   bytes32 queryId = testRandom();
+
+
+  emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+
+}
+
+// //FOR TESTING ONLY
+// function testRandom() public returns(bytes32){
+//   bytes32 queryId = bytes32(keccak256("test"));
+//   __callback(queryId, "1", bytes("test"));
+//   return queryId;
+
+// }
+
+function flip(uint guess) public payable minimumCost(spend) {
+    require(guess == 0 || guess == 1, "please pick a a whole number between 0 and 1");
+    require(balance > 0, "contract is empty. Please wait for the contract owner to provide liquidity");
+
+    balance = balance.add(msg.value);
+
+    update(guess);
+    finaliseOutcome(msg.sender, msg.value);
+
+
+}
+
+
+function finaliseOutcome(address sender,uint cost) public payable {
+
+  uint expected = players[sender].latestGuess;
+  uint result = players[sender].outcome;
+
+  if(expected == result){
+     uint won = cost.mul(2);
+
+      players[sender].winnings = players[sender].winnings.add(won);
+      players[sender].wins = players[sender].wins.add(1);
+
+      assert(expected == result);
+
+      outcomeMessage = "Congratulations! You have doubled your stake!";
+      emit playersOutcome(won, "Congratulations! You have doubled your stake!");
+
+
+  } else if(expected != result) {
+
+     players[sender].loss = players[sender].loss.add(1);
+
+     assert(expected != result);
+
+    outcomeMessage =  "You have lost your stake!";
+    emit playersOutcome(cost , "You have lost your stake!");
+  }
+
+  emit playerStats(players[sender].winnings, players[sender].wins, players[sender].loss);
+
+  assert(result == 0 || result == 1);
+}
+
+
+function withdraw_Winnings() public {
+    require(players[msg.sender].winnings > 0, "You do not have any winning to withdraw");
+    require(balance >= players[msg.sender].winnings, "Insufficient contract funds for withdrawal. Please wait till the contract has more funds available.");
+
+     msg.sender.transfer(players[msg.sender].winnings);
+     balance -= players[msg.sender].winnings;
+     players[msg.sender].winnings = 0;
+
+
+     assert(balance >= players[msg.sender].winnings);
 }
 
 function addLiquidity() public payable minimumCost(spend) onlyOwner returns(uint) {
--- test/coinfliptest.js
+++ test/coinfliptest.js
@@ -12,47 +12,66 @@ contract("People", async function(accounts){
 
   });
 
+  it("shouldn't allow a flip if the contract balance is 0", async function(){
+
+    await truffleAssert.fails(instance.flip(1, {value: web3.utils.toWei("0.1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+  });
+
 
 it("should allow the owner to add liquidity to the smart contract", async function(){
 
-  await truffleAssert.passes(instance.addLiquidity({value: web3.utils.toWei("1", "ether"), from: accounts[0]}), truffleAssert.ErrorType.REVERT);
+  await truffleAssert.passes(instance.addLiquidity({value: web3.utils.toWei("0.4", "ether"), from: accounts[0]}), truffleAssert.ErrorType.REVERT);
 
 });
 
 it("shouldn't allow non-owners to add liquidity to the smart contract", async function(){
 
-  await truffleAssert.fails(instance.addLiquidity({value: web3.utils.toWei("1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+  await truffleAssert.fails(instance.addLiquidity({value: web3.utils.toWei("0.4", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
 
 });
 
 it("shouldn't allow a number other than 0 or 1 to be guessed", async function(){
 
-  await truffleAssert.fails(instance.guess(2, {value: web3.utils.toWei("1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+  await truffleAssert.fails(instance.flip(2, {value: web3.utils.toWei("1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
 });
 
-it("shouldn't allow a guess for < 1 ETH", async function(){
+it("shouldn't allow a flip for < 0.1 ETH", async function(){
 
-  await truffleAssert.fails(instance.guess(1, {value: web3.utils.toWei("0.5", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+  await truffleAssert.fails(instance.flip(1, {value: web3.utils.toWei("0.05", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
 });
 
-it("should allow a guess for for atleast  >= 1 ETH", async function(){
+it("should allow a flip for for atleast  >= 0.1 ETH", async function(){
 
-  await truffleAssert.passes(instance.guess(1, {value: web3.utils.toWei("1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+  await truffleAssert.passes(instance.flip(1, {value: web3.utils.toWei("0.1", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
 });
 
-it("shouldn't allow a guess if the staking amount > contract balance", async function(){
-
-  await truffleAssert.fails(instance.guess(1, {value: web3.utils.toWei("30", "ether"), from: accounts[1]}), truffleAssert.ErrorType.REVERT);
-});
-
-it("should ensure outcome of the guess is either 0 or 1", async function(){
+it("should ensure outcome of the flip is either 0 or 1", async function(){
 
   let outcome;
 
-  await instance.guess(1, {value: web3.utils.toWei("1", "ether"), from: accounts[1]});
+  await instance.flip(1, {value: web3.utils.toWei("0.1", "ether"), from: accounts[1]});
   outcome = await instance.outcome.call().then(function(res){return res; });
   assert(outcome == 0 || outcome == 1, "outcome is not 0 or 1");
 });
+
+it("should allow user to withdraw all their winnings if winnings > 0, user winnings should go to 0 after withdrawal", async function(){
+  let winnings = await instance.getPlayer.call().then(function(rest){res[0]});
+
+  if(winnings > 0){
+    await instance.withdraw_Winnings({from: accounts[0]});
+    winnings = await instance.getPlayer.call().then(function(rest){res[0]});
+    assert(winnings == 0, "winnings didn't reset to 0. withdrawal unsuccessful")
+  }
+});
+
+it("should ensure blockchain contract balance and local contract balance are the same", async function(){
+  let balance_fromLocal = await instance.balance.call().then.(function(res){return res; });
+  let balance_fromBlockchain = await web3.eth.getBalance(instance.address); //returns a string
+
+  assert(balance_fromLocal.toString() === balance_fromBlockchain, "local contract balance and blockchain balances don't match");
+
+});
+
 
 it("shouldn't allow a non-owner to destroy the contract", async function(){
   await truffleAssert.fails(instance.destroy({from: accounts[1]}));
